89c23179177a17ab285299a0d399b4112ec20bf3
Mahesh Kumar <81576386+jacktheboss220@users.noreply.github.com>
2022-06-05 12:56:52 +0530

    Delete plugins directory

--- plugins/quote.js
+++
@@ -1,9 +0,0 @@
-
-const axios = require("axios");
-
-module.exports.getQuote = async () => {
-    let url = "https://zenquotes.io/api/random";
-    let { data } = await axios.get(url);
-    let quote = data[0].q;
-    return quote;
-};
